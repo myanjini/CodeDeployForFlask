6bae1609b09618a1db26736a276d36b4daaf1226
Park Changryum <myanjini@gmail.com>
2023-11-17 13:25:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'hello flask & code deploy V3'
+    return 'hello flask & code deploy V4'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
